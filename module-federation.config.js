8cc3caa9e347e81100c13452fb39c6fdba48356e
prabhakarikonicit <prabhakar.koppireddy@ikonicit.com>
2025-05-20 01:58:11 +0530

    Update module-federation.config.js

--- module-federation.config.js
+++ module-federation.config.js
@@ -1,8 +1,12 @@
 export const mfConfig = {
   name: "dhaam_store_app_ui",
+  filename: "remoteEntry.js",
   exposes: {
     "./Stores": "./src/localComponents/stores", 
     "./tailwindStyles": "./src/index.css"
   },
   shared: ["react", "react-dom"],
+  types: {
+      skipEmit: process.env.NODE_ENV === 'development',
+    },
 };
